b125d42133e5dd961909c6671d0e0daaf9630bfe
Вагин Ростислав <rostwik@gmail.com>
2019-09-06 22:47:47 +0300

    Рассчитал для Москвы.

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -13,8 +13,12 @@ sites = {
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 distances = {}
+distances['Moscow'] = {}
+distances['Moscow']['London'] = ((sites['Moscow'][0]-sites['London'][0]) ** 2 + (sites['Moscow'][1]-sites['London'][1]) ** 2) ** 0.5
+distances['Moscow']['Paris'] = ((sites['Moscow'][0]-sites['Paris'][0]) ** 2 + (sites['Moscow'][1]-sites['Paris'][1]) ** 2) ** 0.5
 
-# TODO здесь заполнение словаря
+
+
 
 print(distances)
 
