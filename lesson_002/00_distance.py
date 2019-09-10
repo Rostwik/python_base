40c7ee0ef5d03ba100004371e45dc5b66fe22ce0
Вагин Ростислав <rostwik@gmail.com>
2019-09-10 21:38:32 +0300

    Доработал, попытка сдачи №3.

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -27,19 +27,15 @@ moscow_paris = (((sites_moscow[0] - sites_paris[0]) ** 2
                  + (sites_moscow[1] - sites_paris[1]) ** 2) ** 0.5)
 
 distances['London'] = {}
-# TODO london_moscow будет равен moscow_london, как и moscow_paris равен paris_moscow
-# TODO всего должно остаться 3 переменных с вычислениями
-london_moscow = ((sites_london[0] - sites_moscow[0]) ** 2 + (
-            sites_london[1] - sites_moscow[1]) ** 2) ** 0.5
+
+london_moscow = moscow_london
 london_paris = ((sites_london[0] - sites_paris[0]) ** 2 + (
             sites_london[1] - sites_paris[1]) ** 2) ** 0.5
 
 distances['Paris'] = {}
 
-paris_moscow = ((sites_paris[0] - sites_moscow[0]) ** 2 + (
-            sites_paris[1] - sites_moscow[1]) ** 2) ** 0.5
-paris_london = ((sites_paris[0] - sites_london[0]) ** 2 + (
-            sites_paris[1] - sites_london[1]) ** 2) ** 0.5
+paris_moscow = moscow_paris
+paris_london = london_paris
 
 distances['Moscow']['London'] = moscow_london
 distances['Moscow']['Paris'] = moscow_paris
