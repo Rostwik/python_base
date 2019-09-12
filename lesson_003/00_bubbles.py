b85a3a05c5182232ce69747989c689b4fa5a847c
Вагин Ростислав <rostwik@gmail.com>
2019-09-12 22:41:57 +0300

    Сделал 2 todo из bubbles

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -5,10 +5,22 @@ import simple_draw as sd
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
 
-# Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+point = sd.get_point(200, 200)
+
+for i in range(0, 15, 5):
+    sd.circle(center_position=point, radius=50 + i, color=(125, 0, 250), width=1)
+
+# Написать функцию рисования пузырька, принимающую 2 (или более) параметра: точка рисовании и шаг
+
+def circle_ex(point_cir, step):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(center_position=point_cir, radius=radius)
+
+point = sd.get_point(400, 200)
+circle_ex(point, 10)
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
@@ -20,5 +32,3 @@ sd.resolution = (1200, 600)
 # TODO здесь ваш код
 
 sd.pause()
-
-
