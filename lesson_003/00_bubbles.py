5ebcf4a688769eadba58aa09ee53da32979e5524
Вагин Ростислав <rostwik@gmail.com>
2019-09-15 20:14:49 +0300

    Сделал реформат, но 15 все равно подчеркнуто. Что я не так делаю? Последнюю TODO не понял, я использую стандартную функцию, там нет step.

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -11,37 +11,37 @@ point = sd.get_point(100, 100)
 for i in range(0, 15, 5):
     sd.circle(center_position=point, radius=50 + i, color=(125, 0, 250), width=1)
 
+
 # Написать функцию рисования пузырька, принимающую 2 (или более) параметра: точка рисовании и шаг
-# TODO используйте Code/ Reformat Code, чтобы исправить стилистические ошибки
-def circle_ex(point_cir, step):  # TODO нужен ещё параметр для передачи цвета в sd.circle()
+
+def circle_ex(point_cir, step, color):
     radius = 50
     for _ in range(3):
         radius += step
-        sd.circle(center_position=point_cir, radius=radius)
+        sd.circle(center_position=point_cir, radius=radius, color=sd.random_color())
+
 
 point = sd.get_point(400, 200)
-circle_ex(point, 10)
+circle_ex(point_cir=point, step=10, color=sd.random_color())
 
 # Нарисовать 10 пузырьков в ряд
 
-for x in range(200,1200,100):
+for x in range(200, 1200, 100):
     point = sd.get_point(x, 550)
-    circle_ex(point_cir=point, step=5)
-
+    circle_ex(point_cir=point, step=5, color=sd.random_color())
 
 # Нарисовать три ряда по 10 пузырьков
 
 
-for x in range(200,1200,100):
-    for y in range(200,500,100):
+for x in range(200, 1200, 100):
+    for y in range(200, 500, 100):
         point = sd.get_point(x, y)
-        circle_ex(point_cir=point, step=8)
-
+        circle_ex(point_cir=point, step=8, color=sd.random_color())
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 
 for _ in range(100):
-    point = sd.random_point()  # TODO а здесь добавить выбор случайного цвета для каждого из них
-    sd.circle(center_position=point,radius=10)  # TODO в параметрах не было радиуса (был step)
+    point = sd.random_point()
+    sd.circle(center_position=point, radius=10, color=sd.random_color())
 
 sd.pause()
