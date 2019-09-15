d19c79168b2a0a097501f80083c7441ac8af3f58
Вагин Ростислав <rostwik@gmail.com>
2019-09-15 21:40:09 +0300

    00 Сделал реформат, но 15 все равно подчеркнуто. Что я не так делаю? Последнюю TODO не понял, я использую стандартную функцию, там нет step. 01 Исправил. 02 Исправил. Даже не подумал о такой возможности( 03 Исправил. 04 Исправил. 05 Исправил. 06 Исправил. 07 Спасибо за совет. Так намного лаконичнее.

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -7,25 +7,15 @@ import simple_draw as sd
 # Использовать вложенные циклы for
 
 sd.resolution = (600, 600)
-delta = 0
-# TODO хорошая работа! Но стиль можно красиво упростить
-# TODO вместо ручного счёта рядов - добавьте enumerate() к первому циклу
-# TODO что-то вроде: фор дельта, игрик ин энумерэйт(рэндж(..)):
-for y in range(0, 700, 50):
-    delta += 1
-    flag = delta % 2
+
+for delta, y in enumerate(range(0, 700, 50)):
     for x in range(0, 700, 100):
-        # TODO вместо флаг можно просто вставлять условие delta % 2 - будет понятно и просто
-        # TODO внутри практически одинаковые записи, меняются только переменные
-        # TODO можно использовать короткую запись условия для изменения одной лишь переменной
-        # TODO х0 равен х - 50 если дельта % 2 в ином случае х
-        # TODO а дальше брать точки используя х0
-        if flag:
-            l_point = sd.get_point(x - 50, y)
-            t_point = sd.get_point(x + 50, y + 50)
+        if delta % 2:
+            x0 = x - 50
         else:
-            l_point = sd.get_point(x, y)
-            t_point = sd.get_point(x + 100, y + 50)
+            x0 = x
+        l_point = sd.get_point(x0, y)
+        t_point = sd.get_point(x0 + 100, y + 50)
         sd.rectangle(left_bottom=l_point, right_top=t_point, width=2)
 
 sd.pause()
