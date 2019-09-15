6c7769aa4bb4fb066f36ee5132b39102432416f5
Вагин Ростислав <rostwik@gmail.com>
2019-09-15 22:16:22 +0300

    00 Сделал реформат, но 15 все равно подчеркнуто. Что я не так делаю? Последнюю TODO не понял, я использую стандартную функцию, там нет step. 01 Исправил. 02 Исправил. Даже не подумал о такой возможности( 03 Исправил. 04 Исправил. 05 Исправил. 06 Исправил. 07 Спасибо за совет. Так намного лаконичнее. 08 Исправил.

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -3,10 +3,10 @@
 # (определение функций)
 
 import simple_draw as sd
-# TODO пройдитесь Code/ Reformat code, чтобы исправить стилистические ошибки
+
 
 def smile(x, y, color):
-    lb_el = sd.get_point(x , y)
+    lb_el = sd.get_point(x, y)
     rt_el = sd.get_point(x + 200, y + 170)
     sd.ellipse(left_bottom=lb_el, right_top=rt_el, color=color, width=1)
     x += 50
@@ -16,9 +16,9 @@ def smile(x, y, color):
     x += 100
     cp = sd.get_point(x, y)
     sd.circle(center_position=cp, radius=15, color=color)
-    l_smile = sd.get_point(x-120,y-60)
+    l_smile = sd.get_point(x - 120, y - 60)
     mid_l_smile = sd.get_point(x - 90, y - 80)
-    mid_r_smile = sd.get_point(x-20, y - 80)
+    mid_r_smile = sd.get_point(x - 20, y - 80)
     r_smile = sd.get_point(x + 20, y - 60)
     smile_list = [l_smile, mid_l_smile, mid_r_smile, r_smile]
     sd.lines(point_list=smile_list, color=color, closed=False, width=1)
@@ -26,17 +26,26 @@ def smile(x, y, color):
 
 sd.resolution = (1200, 600)
 
-
 for _ in range(10):
-    sr = sd.random_point()  # TODO для рандом колора тоже лучше отдельную переменную выделить :)
-    # TODO + желательно ограничить значения, а то смайлики сильно вылезают за границы
-    smile(sr.x, sr.y, sd.random_color())
-
+    sr = sd.random_point()
+    rnd_color = sd.random_color()
+    if sr.x > 1000:
+        x = 1000
+    else:
+        x = sr.x
+    if sr.y > 430:
+        y = 430
+    else:
+        y = sr.y
+    smile(x, y, rnd_color)
+    print (x, y)
 
 
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
+
+#smile(0, 430, (255, 0, 120))
 
 sd.pause()
