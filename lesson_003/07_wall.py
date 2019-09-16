160174c43c3cc56d1d550e2d3f3283af05eb84b8
Вагин Ростислав <rostwik@gmail.com>
2019-09-16 21:50:32 +0300

    00: Подскажите, пожалуйста, почему pc подчеркнул "15" в 11 строке? 07: Крутая фишка, спасибо.

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -10,12 +10,7 @@ sd.resolution = (600, 600)
 
 for delta, y in enumerate(range(0, 700, 50)):
     for x in range(0, 700, 100):
-        if delta % 2:  # TODO в таких ситуация можно использовать крутое сокращение
-            # TODO x0 = x - 50 if delta % 2 else x - одна изящная строка заменит 4 :) настоящий пайтон-стайл
-            # TODO не то, чтобы это ошибка - просто захотелось показать такую фишку, пока есть другие правки)
-            x0 = x - 50
-        else:
-            x0 = x
+        x0 = x - 50 if delta % 2 else x
         l_point = sd.get_point(x0, y)
         t_point = sd.get_point(x0 + 100, y + 50)
         sd.rectangle(left_bottom=l_point, right_top=t_point, width=2)
