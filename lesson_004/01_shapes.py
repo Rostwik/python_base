e63262d61b567644d29ef74151c0ab168f484f76
Вагин Ростислав <rostwik@gmail.com>
2019-09-22 22:42:23 +0300

    Исходный комит, отображение всех фигур.

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # Часть 1.
 # Написать функции рисования равносторонних геометрических фигур:
 # - треугольника
@@ -27,7 +28,97 @@ import simple_draw as sd
 # sd.line()
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
-# TODO здесь ваш код
+# определить функцию рисования треугольника из заданной точки с заданным наклоном
+
+def triangle(point, length, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=1)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=1)
+    v3.draw()
+
+    sd.line(start_point=point, end_point=v3.end_point)
+
+
+def square(point, length, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=1)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=1)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 270, length=length, width=1)
+    v4.draw()
+
+    sd.line(start_point=point, end_point=v4.end_point)
+
+
+def pentagon(point, length, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=1)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=1)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=1)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 288, length=length, width=1)
+    v5.draw()
+
+    sd.line(start_point=point, end_point=v5.end_point)
+
+
+def hexagon(point, length, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=1)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=1)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=1)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=1)
+    v5.draw()
+
+    v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=1)
+    v6.draw()
+
+    sd.line(start_point=point, end_point=v6.end_point)
+
+
+length = 100
+point = sd.get_point(100, 100)
+
+triangle(point=point, length=length, angle=30)
+
+length = 100
+point = sd.get_point(400, 100)
+
+square(point=point, length=length, angle=30)
+
+length = 100
+point = sd.get_point(100, 350)
+
+pentagon(point=point, length=length, angle=30)
+
+length = 100
+point = sd.get_point(400, 350)
+
+hexagon(point=point, length=length, angle=30)
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
--- lesson_004/practice/01_triangle.py
+++ lesson_004/practice/01_triangle.py
@@ -8,14 +8,14 @@ import simple_draw as sd
 length = 200
 point = sd.get_point(300, 300)
 
-# v1 = sd.get_vector(start_point=point, angle=0, length=200, width=3)
-# v1.draw()
-#
-# v2 = sd.get_vector(start_point=v1.end_point, angle=120, length=200, width=3)
-# v2.draw()
-#
-# v3 = sd.get_vector(start_point=v2.end_point, angle=240, length=200, width=3)
-# v3.draw()
+v1 = sd.get_vector(start_point=point, angle=0, length=200, width=3)
+v1.draw()
+
+v2 = sd.get_vector(start_point=v1.end_point, angle=120, length=200, width=3)
+v2.draw()
+
+v3 = sd.get_vector(start_point=v2.end_point, angle=240, length=200, width=3)
+v3.draw()
 
 # определить функцию рисования треугольника из заданной точки с заданным наклоном
 def triangle(point, angle=0):
