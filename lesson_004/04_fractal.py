3bbcec65d5397d5bc3c4f484b5c28dc2a0a43dd9
Вагин Ростислав <rostwik@gmail.com>
2019-09-23 22:38:03 +0300

    Расширенное дерево готово. Приятно быть художником.

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -32,9 +32,11 @@ def draw_branches(point, angle, length):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
     v1.draw()
     next_point = v1.end_point
-    next_angle0 = angle - 30
-    next_angle1 = angle + 30
-    next_length = length * .75
+    rnd_angle = sd.random_number(18, 42)
+    rnd_length_kof = sd.random_number(6, 9)
+    next_angle0 = angle - rnd_angle
+    next_angle1 = angle + rnd_angle
+    next_length = length * rnd_length_kof / 10
     draw_branches(point=next_point, angle=next_angle0, length=next_length)
     draw_branches(point=next_point, angle=next_angle1, length=next_length)
 
