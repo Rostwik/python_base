f124e0b17ca0646af11b2f55e608297b6906c3cd
Вагин Ростислав <rostwik@gmail.com>
2019-09-24 23:23:58 +0300

    05 Осталось придумать сугроб.

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -2,13 +2,52 @@
 
 import simple_draw as sd
 
-
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+# N = [[(x,y), n]]
+
+sd.resolution = (1200, 600)
+
+N = []
+
+for _ in range(20):
+    x = sd.random_number(50, 1150)
+    n = sd.random_number(10, 50)
+    N.append([x, n])
+
+print(N)
+y = 600
+
+while True:
+    sd.start_drawing()
+    delta = sd.random_number(2, 10)
+    plus_minus = sd.random_number(0, 1)
+    for i in N:
+
+        point = sd.get_point(i[0], y)
+        sd.snowflake(center=point, length=i[1], color=sd.background_color)
+    y -= 5
+    for i in N:
+        point = sd.get_point(i[0], y)
+        sd.snowflake(center=point, length=i[1], color=sd.COLOR_WHITE)
+        if plus_minus:
+            i[0] += delta
+        else:
+            i[0] -= delta
+
+# i[0] += delta if plus_minus else i[0]-=delta TODO: в таком формате PCh подчеркивает красным выражение после else,
+#                                               сигнализируя об ошибочном синтаксисе. Подскажите, пожалуйста,
+#                                               почему так происходит?
+    sd.finish_drawing()
+    sd.sleep(0.1)
+
+
+    sd.clear_screen()
+    if y < 10:
+        break
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,15 +56,35 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+# познакомится с параметрами библиотечной функции рисования снежинки sd.snowflake()
+
+# sd.snowflake(center=point_0, length=200, factor_a=0.5)
+
+# y = 500
+# x = 100
+#
+# y2 = 450
+# x2 = 150
+# while True:
+#     sd.clear_screen()
+#     point = sd.get_point(x, y)
+#     sd.snowflake(center=point, length=50)
+#     y -= 10
+#     if y < 50:
+#         break
+#     x = x + 10
+#
+#     point2 = sd.get_point(x2, y2)
+#     sd.snowflake(center=point2, length=30)
+#     y2 -= 10
+#     if y2 < 50:
+#         break
+#     x2 = x2 + 20
+#
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
+
 
 sd.pause()
 
@@ -43,5 +102,3 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
-
-
