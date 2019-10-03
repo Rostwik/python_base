9366d3aff79434cc9a59040c4068645c89d6d496
Вагин Ростислав <rostwik@gmail.com>
2019-10-03 21:29:30 +0300

    05 01 попытка 2

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,12 +3,12 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-import room_1
-import room_2
+import lesson_005.room_1 as room_1
+import lesson_005.room_2 as room_2
 
 # Подскажите, пожалуйста, почему, room подчеркнуто красным.
-# TODO Пайтон не может найти этот модуль, ему нужно помочь, уточнив путь
-# TODO import lesson_005.room_1 и тд
+
+
 print('В комнате room_1 живут:', ', '.join(room_1.folks))
 print('В комнате room_2 живут:', ', '.join(room_2.folks))
 
