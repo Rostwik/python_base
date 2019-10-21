69d690268beb0c76cf8492ae28773e446d5633ac
Вагин Ростислав <rostwik@gmail.com>
2019-10-21 21:24:37 +0300

    Человек и кошка v.1

--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from random import randint
+from random import randint, choice
 
 
 # Доработать практическую часть урока lesson_007/python_snippets/08_practice.py
@@ -27,15 +27,124 @@ from random import randint
 # Человеку и коту надо вместе прожить 365 дней.
 
 class Cat:
-    pass
+    def __init__(self):
+        self.satiety = 0
+        self.house = None
+        self.nickname = None
+
+    def sleep(self):
+        self.satiety -= 10
+        print('Кот храпит!')
+
+    def eat(self):
+        self.satiety += 20
+        self.house.cat_food -= 10
+        print('Котик чавкает!')
+
+    def tear_wallpaper(self):
+        self.satiety -= 10
+        self.house.mud += 5
+        print('Кот элегантно спускается вниз по новеньким обоям...')
+
+    def status(self):
+        print('Сытость кота', self.satiety)
+
+    def act(self):
+
+        if self.satiety < 20:
+            self.eat()
+        elif self.satiety < 40:
+            self.sleep()
+        else:
+            self.tear_wallpaper()
+        dice = randint(1, 6)
+        if dice in [1, 3]:
+            self.eat()
 
 
 class Human:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.house = None
+        self.satiety = 30
+        self.money = 300
+        print('Меня зовут', self.name, 'У меня нет крыши над головой :(')
+
+    def buy_house(self, house):
+        self.house = house
+        print('Я купил дом, ура!')
+
+    def shelter_a_pet(self, other, nickname):
+        other.house = self.house
+        other.nickname = nickname
+        print('Я приютил котика по имени', nickname)
+
+    def buy_cat_food(self):
+        self.house.cat_food += 50
+        self.money -= 50
+        print('Котику нечего кушать, надо сходить в Пятерочку, я знаю там сегодня акция на Шебу!')
+
+    def buy_food(self):
+        self.house.food += 50
+        self.money -= 70
+        print('В холодильнике пусто, схожу в магазин!')
+
+    def clean_the_house(self):
+        self.house.mud -= 100
+        self.satiety -= 20
+        print('Опять кот все разбросал :(, убираю!')
+
+    def work(self):
+        self.money += 150
+        print('Программа сама себя не напишет! Доделаю проект, авось заплатят сразу.')
+
+    def eat(self):
+        self.satiety += 20
+
+    def status(self):
+        print('Моя сытость:', self.satiety, 'Денег у меня:', self.money, 'В доме грязно на:', self.house.mud)
+
+    def act(self):
+        if self.house.food < 100:
+            self.buy_food()
+        elif self.house.cat_food < 80:
+            self.buy_cat_food()
+        elif self.house.mud > 120:
+            self.clean_the_house()
+        elif self.money < 100:
+            self.work()
+        elif self.satiety < 40:
+            self.eat()
 
 
 class House:
-    pass
+    streets = ['Komsomolskaya', 'Sovetskaya', 'Pionerskaya', 'Lelinskaya']
+
+    def __init__(self):
+        self.house_street = choice(House.streets)
+        self.food = 40
+        self.cat_food = 0
+        self.mud = 0
+
+
+my_house = House()
+good_person = Human('Вова')
+good_person.buy_house(my_house)
+kitten = Cat()
+good_person.shelter_a_pet(kitten, 'Persik')
+
+
+for day in range(1, 366):
+    print('================ день {} =================='.format(day))
+    kitten.act()
+    good_person.act()
+
+    print('--- в конце дня ---')
+    kitten.status()
+    good_person.status()
+    if kitten.satiety < 0:
+        print('Котик умер (((((((((')
+        break
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
