7f2d102cd378c0f4006653c92b06fc6a4bca3347
Вагин Ростислав <rostwik@gmail.com>
2019-11-11 23:17:12 +0300

    Статист Начало.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -20,8 +20,31 @@
 #
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+import zipfile
 
-# TODO здесь ваш код
+
+class Statist:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def collect(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(file=self.file_name, 'r', encoding='cp1251'):
+            for line in file:
+                self._collect_in_line(line)
+
+    def _collect_in_line(self, line):
+        for char in line:
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
