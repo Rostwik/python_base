4c3ac2e3309ec1dc7d6eb01dcae98a9b75bbda43
Вагин Ростислав <rostwik@gmail.com>
2019-11-12 23:01:04 +0300

    Статист v1.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -24,6 +24,7 @@ import zipfile
 
 
 class Statist:
+    total = 0
 
     def __init__(self, file_name):
         self.file_name = file_name
@@ -38,13 +39,51 @@ class Statist:
     def collect(self):
         if self.file_name.endswith('.zip'):
             self.unzip()
-        with open(file=self.file_name, 'r', encoding='cp1251'):
+        with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
                 self._collect_in_line(line)
 
     def _collect_in_line(self, line):
         for char in line:
+            if char.isalpha():
+                if char in self.stat:
+                    self.stat[char] += 1
+                    self.total += 1
+                else:
+                    self.stat[char] = 1
+                    self.total += 1
 
+    def display_stat(self, list_count):
+        print('+{txt:-^18}+'.format(txt='+'))
+        print('|{:^8}|{:^9}|'.format('Буква', 'Частота'))
+        print('+{txt:-^18}+'.format(txt='+'))
+        for char, count in list_count:
+            print('|{simbol:^8}|{frequency:^9}|'.format(simbol=char, frequency=count))
+        print('+{txt:-^18}+'.format(txt='+'))
+        print('|{:^8}|{:^9}|'.format('Итого', self.total))
+        print('+{txt:-^18}+'.format(txt='+'))
+
+    def frequency_ascending(self):
+        list_count_of_simbols = list(self.stat.items())
+        list_count_of_simbols.sort(key=lambda list_count_of_simbols: list_count_of_simbols[1])
+        self.display_stat(list_count_of_simbols)
+
+    def alphabetically_ascending(self):
+        list_count_of_simbols = sorted(self.stat.items())
+        self.display_stat(list_count_of_simbols)
+
+    def alphabetically_descending(self):
+        list_count_of_simbols = sorted(self.stat.items(), reverse=True)
+        self.display_stat(list_count_of_simbols)
+
+
+count_of_simbols = Statist('C:\\Users\\Авито\\PycharmProjects\\python_base'
+                           '\\lesson_009\\python_snippets\\voyna-i-mir.txt.zip')
+
+count_of_simbols.collect()
+count_of_simbols.alphabetically_ascending()
+count_of_simbols.alphabetically_descending()
+count_of_simbols.frequency_ascending()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
