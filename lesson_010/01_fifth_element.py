df1ec07174783c62be7b8886d68980e4abfdae6a
Вагин Ростислав <rostwik@gmail.com>
2019-11-24 20:48:10 +0300

    5 Элемент. Начало.

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -2,19 +2,27 @@
 
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
 
-BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    BRUCE_WILLIS = 42
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
+except ValueError:
+    print('Невозможно преобразовать к числу')
+except IndexError:
+    print('Выход за границы списка')
+except:
+    print('Другие ошибки.')
+
+
+
+
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
-
