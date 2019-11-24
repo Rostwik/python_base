322a7d1665f6500b29e8721fff6eb8cf5c6ae0a3
Вагин Ростислав <rostwik@gmail.com>
2019-11-25 01:04:41 +0300

    День сурка v.1

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -17,8 +17,64 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 
-ENLIGHTENMENT_CARMA_LEVEL = 777
+from random import randint
 
-# TODO здесь ваш код
+
+class IamGodError(Exception):
+    pass
+
+
+class DrunkError(Exception):
+    pass
+
+
+class CarCrashError(Exception):
+    pass
+
+
+class GluttonyError(Exception):
+    pass
+
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
+
+def one_day():
+    exception_dict = {1: IamGodError, 2: DrunkError, 3: CarCrashError, 4: GluttonyError,
+                      5: DepressionError, 6: SuicideError}
+
+    karma = randint(1, 7)
+    dice = randint(1, 13)
+    if dice == 13:
+        dice = randint(1, 6)
+        raise exception_dict[dice]
+    return karma
+
+
+ENLIGHTENMENT_CARMA_LEVEL = 0
+
+while True:
+    try:
+        karma = one_day()
+        ENLIGHTENMENT_CARMA_LEVEL += karma
+        if ENLIGHTENMENT_CARMA_LEVEL >= 777:
+            break
+    except IamGodError:
+        print('Билл думает о вечном.')
+    except DrunkError:
+        print('Билл напился.')
+    except CarCrashError:
+        print('Билл гоняет на машине по железнодорожным путям.')
+    except GluttonyError:
+        print('Билл не боится холестерина.')
+    except DepressionError:
+        print('Билл не хочет вставать из кроватки.')
+    except SuicideError:
+        print('Билл опустил тостер в ванну.')
 
 # https://goo.gl/JnsDqu
