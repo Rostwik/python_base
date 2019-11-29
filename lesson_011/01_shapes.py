eeb4e72fe39f32bb2d76baf65e87d1463774bdbc
Вагин Ростислав <rostwik@gmail.com>
2019-11-29 20:37:36 +0300

    Shapes by func slyle v.1

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # На основе вашего кода из решения lesson_004/01_shapes.py сделать функцию-фабрику,
 # которая возвращает функции рисования треугольника, четырехугольника, пятиугольника и т.д.
 #
@@ -14,12 +15,19 @@ import simple_draw as sd
 
 
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    def draw_any_figure(start_point_figure, length, angle):
+        start_point_vector = start_point_figure
+        side = int(360 / n)
+        for figure_angle in range(0, 360 - side, side):
+            v = sd.get_vector(start_point=start_point_vector, angle=figure_angle + angle, length=length, width=1)
+            v.draw()
+            start_point_vector = v.end_point
+        sd.line(start_point=start_point_figure, end_point=start_point_vector)
+
+    return draw_any_figure
 
 
 draw_triangle = get_polygon(n=3)
-draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
-
+draw_triangle(start_point_figure=sd.get_point(200, 200), angle=13, length=100)
 
 sd.pause()
