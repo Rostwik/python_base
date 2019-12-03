eb11bf8138e009fbeea660d48bc7bc799fd0c722
Вагин Ростислав <rostwik@gmail.com>
2019-12-04 02:41:02 +0300

    Генератор v.1

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -6,12 +6,38 @@
 #
 # пример использования:
 #
-# grouped_events = <создание итератора/генератора>
-# for group_time, event_count in grouped_events:
-#     print(f'[{group_time}] {event_count}')
+
 #
 # на консоли должно появится что-то вроде
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+
+def log_generator():
+    with open('events.txt', 'r') as file:
+        stat = {}
+
+        for line in file:
+
+            if 'NOK' in line:
+                out_log_format = line[1: 17] + line[28:]
+                if out_log_format[1:16] in stat:
+                    stat[out_log_format[1:16]] += 1
+                else:
+                    if stat:
+                        yield list(stat.items())[0]
+                        stat = {}
+                        stat[out_log_format[1:16]] = 1
+                    else:
+                        stat[out_log_format[1:16]] = 1
+        if stat:
+            yield list(stat.items())[0]
+
+
+# TODO Долго думал ничего не придумал. Не покидает чувство беспокойства, что код кривой и можно проще.
+# TODO Последние две строчки добавлены, потому, что последние данные остаются в словаре. Не догадался, прошу содействия.
+
+
+grouped_events = log_generator()
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
