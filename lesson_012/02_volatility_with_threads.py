2db40da9d9e7e1901ae3f687d47c825a50ffdff4
Вагин Ростислав <rostwik@gmail.com>
2019-12-22 20:33:58 +0300

    Volatility Thread v.0.01

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -2,6 +2,7 @@
 
 
 import os
+import threading
 from pprint import pprint
 
 import time
@@ -17,67 +18,80 @@ def time_track(func):
         elapsed = round(ended_at - started_at, 4)
         print(f'Функция работала {elapsed} секунд(ы)')
         return result
+
     return surrogate
 
 
-class Volatility:
+def file_path(data_directory):
+    list_file_paths = []
+    for tup in os.walk(data_directory):
+        for file in tup[2]:
+            if 'csv' in file:
+                filepath = os.path.join(data_directory, file)
+                list_file_paths.append(filepath)
+    return list_file_paths
 
-    def __init__(self, input_dir):
-        self.data_directory = input_dir
-        self.list_file_paths = []
-        self.volatility = []
-        self.volatility_0 = []
+
+def display_result(tikers):
+    volatility = []
+    volatility_0 = []
+
+    for tiker in tikers:
+        if not tiker.volatility:
+            volatility_0.append(tiker.secid)
+        else:
+            volatility.append([tiker.secid, tiker.volatility])
+    volatility.sort(key=lambda list_vol: list_vol[1], reverse=True)
+
+    print('Максимальная волатильность:')
+    for index in volatility[0:3]:
+        print(f'     {index[0]:} - {index[1]:3.2f} %')
+    print('Минимальная волатильность:')
+    for index in volatility[:-4:-1][::-1]:
+        print(f'     {index[0]:} - {index[1]:3.2f} %')
+    print('Нулевая волатильность:')
+    volatility_0.sort()
+    print('    ', end=' ')
+    print(', '.join(volatility_0))
+
+
+class Volatility(threading.Thread):
+
+    def __init__(self, filepath):
+        super().__init__()
         self.minmax_list = []
+        self.volatility = 0
+        self.secid = ''
+        self.filepath = filepath
 
-    def display_result(self):
-        print('Максимальная волатильность:')
-        for index in self.volatility[0:3]:
-            print(f'     {index[0]:} - {index[1]:3.2f} %')
-        print('Минимальная волатильность:')
-        for index in self.volatility[:-4:-1][::-1]:
-            print(f'     {index[0]:} - {index[1]:3.2f} %')
-        print('Нулевая волатильность:')
-        self.volatility_0.sort()
-        print('    ', end=' ')
-        print(', '.join(self.volatility_0))
-
-    def collect_data_from_files(self, file):
-        with open(file, 'r') as tiker_file:
+    def collect_data_from_files(self):
+        with open(self.filepath, 'r') as tiker_file:
             tiker_file.readline()
             for line in tiker_file:
                 secid, tradetime, price, quantity = line.split(',')
                 self.minmax_list.append(float(price))
-        return secid
-
-    def file_path(self):
-        for tup in os.walk(self.data_directory):
-            for file in tup[2]:
-                if 'csv' in file:
-                    filepath = os.path.join(self.data_directory, file)
-                    self.list_file_paths.append(filepath)
+        self.secid = secid
 
     def run(self):
-        self.file_path()
-        for file in self.list_file_paths:
-            self.minmax_list = []
-            secid = self.collect_data_from_files(file)
-            max_vol = max(self.minmax_list)
-            min_vol = min(self.minmax_list)
-            average_price = (max_vol + min_vol) / 2
-            delta = max_vol - min_vol
-            volatility = (delta / average_price) * 100
-            if not volatility:
-                self.volatility_0.append(secid)
-            else:
-                self.volatility.append([secid, volatility])
-        self.volatility.sort(key=lambda list_vol: list_vol[1], reverse=True)
-        self.display_result()
+        self.collect_data_from_files()
+        max_vol = max(self.minmax_list)
+        min_vol = min(self.minmax_list)
+        average_price = (max_vol + min_vol) / 2
+        delta = max_vol - min_vol
+        self.volatility = (delta / average_price) * 100
 
 
 @time_track
 def main():
-    success = Volatility('trades')
-    success.run()
+    list_file_paths = file_path('trades')
+    tikers = [Volatility(filepath=filepath) for filepath in list_file_paths]
+
+    for tiker in tikers:
+        tiker.start()
+    for tiker in tikers:
+        tiker.join()
+
+    display_result(tikers)
 
 
 if __name__ == '__main__':
