b1d342b40e3a01b90205d192642f47eb66848639
bi4ok <bi4ok23@gmail.com>
2019-12-31 12:00:03 +0300

    lesson 013: нужны правки в 01

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -11,22 +11,24 @@
 from PIL import Image, ImageFont, ImageDraw, ImageColor
 
 
-# TODO: Note: you may need to restart the kernel to use updated packages.  Подскажите, пожалуйста, что
-# TODO: означает эта фраза? У меня оущещние, что файл requirements.txt  не обновляется.
-
+# Note: you may need to restart the kernel to use updated packages.  Подскажите, пожалуйста, что
+# означает эта фраза? У меня оущещние, что файл requirements.txt  не обновляется.
+# TODO Фраза означает, что нужна перезагрузка, однако у вас там что-то очень много завизимостей прописано
+# TODO И файл должен быть расположен внутри 13-ого урока, а не снаружи
 def make_ticket(fio, from_, to, date):
     im = Image.open("images/ticket_template.png")
     print(im.format, im.size, im.mode)
 
     im = Image.open("images/ticket_template.png")
     font = ImageFont.truetype('ofont.ru_Baskerville_WGL4_BT.ttf', size=22)
-    # TODO: не срабатывают  atl+shift+enter (
+    # не срабатывают  atl+shift+enter (
+    # TODO Не понял, что не срабатывает?
     im_txt = ImageDraw.Draw(im)
     im_txt.text((45, 130), fio, font=font, fill=ImageColor.colormap['black'])
     im_txt.text((45, 195), from_, font=font, fill=ImageColor.colormap['black'])
     im_txt.text((45, 265), to, font=font, fill=ImageColor.colormap['black'])
     im_txt.text((250, 265), date, font=font, fill=ImageColor.colormap['black'])
-    im.show()
+    im.show()  # TODO Картинку лучше сохранять, через save()
 
 
 passenger = 'Иванов П.А.'
