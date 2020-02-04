1988bb8cd57450124c222ca804e995458a807b40
Вагин Ростислав <rostwik@gmail.com>
2020-02-04 20:03:09 +0300

    Добавил тесты в бота. Добавились вопросы в тестах бота. Разнообразил тесты боулинга.

--- lesson_014/tests/test_01_score.py
+++ lesson_014/tests/test_01_score.py
@@ -6,31 +6,45 @@ from unittest.mock import Mock
 
 from lesson_014.bowling import get_score
 
-_test_data1 = '2/37X11111111--2821'
+_test_data1 = ''
 _test_data2 = '2/37X19111111--2821'
+_test_data6 = 'XXXXXXXXXX'
 _test_data3 = '2/--X-1XX-/------'
 _test_data4 = '-/-/-/-/-/-/-/-/-/----'
+_test_data5 = '2/37X19111111--282'
+
 
 
 # не могу понять почему запускается только один тест, а не два. Подскажите, пож.
-# TODO Проверка правильных вариантов разных видов - это хорошо, но это только один из вариантов работы программы
-# TODO Старайтесь исходить из 3 основных типов ввода:
-# TODO ничего (пустая строка)
-# TODO много всего(очень длинная строка правильных/неправильных символов)
-# TODO ввод нужного размера (правильный/неправильный)
+#  Проверка правильных вариантов разных видов - это хорошо, но это только один из вариантов работы программы
+#  Старайтесь исходить из 3 основных типов ввода:
+#  ничего (пустая строка)
+#  много всего(очень длинная строка правильных/неправильных символов)
+#  ввод нужного размера (правильный/неправильный)
 class GetScoreTest(unittest.TestCase):
 
     def test_normal(self):
         fake_result = Mock()
         fake_result.text = _test_data1
-        result = get_score(fake_result.text)
-        self.assertEqual(result, 66)
+        get_score(fake_result.text)
 
     def test_normal_2(self):
         fake_result = Mock()
         fake_result.text = _test_data2
         result = get_score(fake_result.text)
         self.assertEqual(result, 74)
+
+    def test_normal_3(self):
+        fake_result = Mock()
+        fake_result.text = _test_data5
+        get_score(fake_result.text)
+
+    def test_normal_4(self):
+        fake_result = Mock()
+        fake_result.text = _test_data6
+        get_score(fake_result.text)
+        result = get_score(fake_result.text)
+        self.assertEqual(result, 200)
 
     def test_hard(self):
         fake_result = Mock()
